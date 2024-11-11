fc53fa9815a304e1e6b1e5ded65901fd324c3446
tanmoypal003 <tanmoypal220@gmail.com>
2024-11-11 15:11:08 +0600

    quiz start button & funtionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import { info } from "./assets/Data";
 
 function App() {
+    let [status, setStatus] = useState(false);
+
+    const loadQuestion = () => {
+        setStatus(true);
+    };
+
     return (
-        <>
-            <div className="quiz_wrapper"></div>
-            <h3>Welcome to the </h3>
-            <h2>QUICK QUIZ</h2>
-            <p>Number of questions : 6</p>
-            <button>Let&apos;s Start</button>
-        </>
+        <main className="container">
+            {status ? (
+                <></>
+            ) : (
+                <>
+                    <div className="quiz_wrapper"></div>
+                    <h3>Welcome to the </h3>
+                    <h2>QUICK QUIZ</h2>
+                    <p>Number of questions : {info.length}</p>
+                    <button onClick={loadQuestion}>Let&apos;s Start</button>
+                </>
+            )}
+
+            {status ? (
+                <>
+                    <div></div>
+                </>
+            ) : (
+                <></>
+            )}
+        </main>
     );
 }
 
