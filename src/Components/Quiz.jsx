1d3a9e017007f9279f482e60924e2b9192a8295a
tanmoypal003 <tanmoypal220@gmail.com>
2024-11-11 20:17:48 +0600

    a question & choices added

---
+++ src/Components/Quiz.jsx
@@ -0,0 +1,17 @@
+function Quiz() {
+    return (
+        <div className="container">
+            <h1>Question</h1>
+            <h2>In OOP, what is inheritance used for?</h2>
+            <ul>
+                <li>Bundling data and methods </li>
+                <li>Achieving code reusability</li>
+                <li>Encapsulating properties</li>
+                <li>Simplifying complex systems</li>
+            </ul>
+            <button>Next</button>
+        </div>
+    );
+}
+
+export default Quiz;
