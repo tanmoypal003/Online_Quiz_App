3620d5ad7d963ce99b7f1aa859e0b7a2f7e1ad6c
tanmoypal003 <tanmoypal220@gmail.com>
2024-11-11 14:58:42 +0600

    landing page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
     return (
         <>
-            <h1>Hello World!</h1>
+            <div className="quiz_wrapper"></div>
+            <h3>Welcome to the </h3>
+            <h2>QUICK QUIZ</h2>
+            <p>Number of questions : 6</p>
+            <button>Let&apos;s Start</button>
         </>
     );
 }
