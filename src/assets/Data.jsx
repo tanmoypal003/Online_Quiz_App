19c4b5cf5da0b2c9b3bd66155b0a9a84e3290af3
tanmoypal003 <tanmoypal220@gmail.com>
2024-11-11 14:53:47 +0600

    quiz questions added

---
+++ src/assets/Data.jsx
@@ -0,0 +1,37 @@
+export const info = [
+    {
+        question: "Where is the biggest seaport in Bangladesh located?",
+        choices: ["Chattogram", "Khulna", "Barisal", "Mongla"],
+        answer: 1,
+    },
+    {
+        question: "How many eyes a human have?",
+        choices: ["2", "3", "4", "1"],
+        answer: 1,
+    },
+    {
+        question: "What is the capital of India?",
+        choices: ["Gujarat", "Kolkata", "Maharastra", "Delhi"],
+        answer: 4,
+    },
+    {
+        question: "Which bird is called 'King of birds'?",
+        choices: ["Kingfisher", "Eagle", "Parrot", "Falcon"],
+        answer: 2,
+    },
+    {
+        question: "What is the national currency of Bangladesh?",
+        choices: ["Rupee", "Dollar", "Taka", "Pound"],
+        answer: 3,
+    },
+    {
+        question: "What is the purpose of a constructor in OOP?",
+        choices: [
+            "To create multiple instances of a class",
+            "To bundle data and methods",
+            "To initialize the properties of an object",
+            "To establish a relationship between classes",
+        ],
+        answer: 1,
+    },
+];
