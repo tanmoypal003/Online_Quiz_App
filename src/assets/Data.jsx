3d74addb4c60cf9491fa0b2fda0cd102a1a02059
tanmoypal003 <tanmoypal220@gmail.com>
2024-11-11 20:51:53 +0600

    questions for quiz updated

--- src/assets/Data.jsx
+++ src/assets/Data.jsx
@@ -1,28 +1,93 @@
 export const info = [
     {
-        question: "Where is the biggest seaport in Bangladesh located?",
-        choices: ["Chattogram", "Khulna", "Barisal", "Mongla"],
-        answer: 1,
+        question: "Which planet is known as the 'Red Planet'?",
+        choices: ["Venus", "Jupiter", "Mars", "Saturn"],
+        answer: 3,
     },
     {
-        question: "How many eyes a human have?",
-        choices: ["2", "3", "4", "1"],
-        answer: 1,
-    },
-    {
-        question: "What is the capital of India?",
-        choices: ["Gujarat", "Kolkata", "Maharastra", "Delhi"],
-        answer: 4,
-    },
-    {
-        question: "Which bird is called 'King of birds'?",
-        choices: ["Kingfisher", "Eagle", "Parrot", "Falcon"],
+        question: "What is the main purpose of encapsulation in OOP?",
+        choices: [
+            "To establish a relationship between classes",
+            "To bundle data and methods into a single unit ",
+            "To achieve code reusability",
+            "To simplify complex systems",
+        ],
         answer: 2,
     },
     {
-        question: "What is the national currency of Bangladesh?",
-        choices: ["Rupee", "Dollar", "Taka", "Pound"],
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        choices: [
+            "Charles Dickens",
+            "Jane Austen",
+            "William Shakespeare",
+            "Mark Twain",
+        ],
         answer: 3,
+    },
+    {
+        question: "In OOP, what is inheritance used for?",
+        choices: [
+            "Bundling data and methods",
+            "Achieving code reusability",
+            "Encapsulating properties",
+            "Simplifying complex systems",
+        ],
+        answer: 2,
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        choices: [
+            "Atlantic Ocean",
+            "Indian Ocean",
+            "Southern Ocean",
+            "Pacific Ocean",
+        ],
+        answer: 4,
+    },
+    {
+        question: "What does the term 'object' refer to in OOP?",
+        choices: [
+            "A variable used for data storage",
+            "A blueprint for creating classes",
+            "An instance of a class",
+            "A method for performing a specific task",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Which country is known as the 'Land of the Rising Sun'?",
+        choices: ["Japan", "China", "India", "South Korea"],
+        answer: 1,
+    },
+    {
+        question: "What is polymorphism in OOP?",
+        choices: [
+            "The ability of a class to have multiple constructors",
+            "The ability of a class to have multiple methods with the same name",
+            "The process of creating multiple instances of a class",
+            "The process of bundling data and methods",
+        ],
+        answer: 2,
+    },
+    {
+        question: "What element does 'O' represent on the periodic table?",
+        choices: ["Osmium", "Ozone", "Omnium", "Oxygen"],
+        answer: 4,
+    },
+    {
+        question: "In OOP, what does the term 'abstraction' refer to?",
+        choices: [
+            "Thomas Jefferson",
+            "George Washington",
+            "Abraham Lincoln",
+            "John Adams",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Who was the first president of the United States?",
+        choices: ["Osmium", "Ozone", "Omnium", "Oxygen"],
+        answer: 4,
     },
     {
         question: "What is the purpose of a constructor in OOP?",
@@ -32,6 +97,21 @@ export const info = [
             "To initialize the properties of an object",
             "To establish a relationship between classes",
         ],
+        answer: 1,
+    },
+    {
+        question: "What is a class in OOP?",
+        choices: [
+            "An instance of an object",
+            "A variable used for data storage",
+            "A blueprint for creating objects",
+            "A method for performing a specific task",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Which keyword is used to achieve method overriding in OOP?",
+        choices: ["override", "extends", "virtual", "super"],
         answer: 1,
     },
 ];
